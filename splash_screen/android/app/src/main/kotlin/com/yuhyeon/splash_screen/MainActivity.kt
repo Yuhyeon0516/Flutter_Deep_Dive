9abc33c22f0a1d36b9d279a0424496fdbd646ae4
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-02 14:25:15 +0900

    Studing splash screen

---
+++ splash_screen/android/app/src/main/kotlin/com/yuhyeon/splash_screen/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.yuhyeon.splash_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ splash_screen/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF335CB0),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("asset/img/splash_potato.png"),
+            const SizedBox(
+              height: 28,
+            ),
+            const CircularProgressIndicator(
+              color: Colors.white,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
