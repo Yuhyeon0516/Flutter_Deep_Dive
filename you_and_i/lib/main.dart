95ddfab80a774a7e356edf7ebd3c0666024f417f
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-03 19:31:49 +0900

    Created home screen UI

--- you_and_i/lib/main.dart
+++ you_and_i/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:you_and_i/screen/home_screen.dart';
 void main() {
   runApp(
     const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     ),
   );
--- you_and_i/lib/screen/home_screen.dart
+++ you_and_i/lib/screen/home_screen.dart
@@ -5,6 +5,68 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.pink[100],
+      body: SafeArea(
+        bottom: false,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    const Text(
+                      "U & I",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "parisienne",
+                        fontSize: 72,
+                      ),
+                    ),
+                    const Text(
+                      "우리 처음 만난날",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "sunflower",
+                        fontSize: 32,
+                      ),
+                    ),
+                    const Text(
+                      "2023.02.11",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "sunflower",
+                        fontSize: 18,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.favorite,
+                      ),
+                      iconSize: 60,
+                      color: Colors.red,
+                    ),
+                    const Text(
+                      "D+1",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "sunflower",
+                        fontSize: 48,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Image.asset("asset/img/middle_image.png"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
