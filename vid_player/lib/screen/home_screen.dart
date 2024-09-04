8fa34bb5f9aa8aa339b2669176d70e84b879ec8b
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-04 21:27:58 +0900

    Created function that select video in gallery.

--- vid_player/lib/screen/home_screen.dart
+++ vid_player/lib/screen/home_screen.dart
@@ -1,33 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool showVideoPlayer = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              Color(0xFF2A3A7C),
-              Color(0xFF000118),
-            ],
-          ),
-        ),
-        child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _Logo(),
-            SizedBox(
-              height: 28,
-            ),
-            _Title(),
+      body: showVideoPlayer
+          ? const _VideoPlayer()
+          : _VideoSelector(onLogoTaped: onLogoTaped),
+    );
+  }
+
+  onLogoTaped() async {
+    final video = await ImagePicker().pickVideo(source: ImageSource.gallery);
+
+    print(video);
+  }
+}
+
+class _VideoSelector extends StatelessWidget {
+  final VoidCallback onLogoTaped;
+  const _VideoSelector({
+    super.key,
+    required this.onLogoTaped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xFF2A3A7C),
+            Color(0xFF000118),
           ],
         ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Logo(
+            onTap: onLogoTaped,
+          ),
+          const SizedBox(
+            height: 28,
+          ),
+          const _Title(),
+        ],
       ),
     );
   }
@@ -66,14 +97,27 @@ class _Title extends StatelessWidget {
 }
 
 class _Logo extends StatelessWidget {
-  const _Logo({
-    super.key,
-  });
+  final VoidCallback onTap;
+  const _Logo({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      'asset/image/logo.png',
+    return GestureDetector(
+      onTap: onTap,
+      child: Image.asset(
+        'asset/image/logo.png',
+      ),
+    );
+  }
+}
+
+class _VideoPlayer extends StatelessWidget {
+  const _VideoPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Video Player"),
     );
   }
 }
