2cc964f4a09b2e022e818d3c2cb3c42104ccdb2c
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-06 14:27:55 +0900

    show my video.

---
+++ video_call/lib/const/keys.dart
@@ -0,0 +1,4 @@
+const appId = '2ad75c6f94e4479c8b4f0e9db763e7d7';
+const channelName = 'testchannel';
+const token =
+    '007eJxTYPALXCMSoS/Av+R+yBaeXf+mh3QurXiRf9Pm9enHdsnPimUVGIwSU8xNk83SLE1STUzMLZMtkkzSDFItU5LMzYxTzVPMD7XdSmsIZGRYcduYhZEBAkF8boaS1OKS5IzEvLzUHAYGAHH8I94=';
--- video_call/lib/screen/cam_screen.dart
+++ video_call/lib/screen/cam_screen.dart
@@ -1,4 +1,7 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:video_call/const/keys.dart';
 
 class CamScreen extends StatefulWidget {
   const CamScreen({super.key});
@@ -8,35 +11,89 @@ class CamScreen extends StatefulWidget {
 }
 
 class _CamScreenState extends State<CamScreen> {
+  RtcEngine? engine;
+  int uid = 0;
+
+  Future<void> init() async {
+    final res = await [Permission.camera, Permission.microphone].request();
+
+    final cameraPermission = res[Permission.camera];
+    final microphonePermission = res[Permission.microphone];
+
+    if (cameraPermission != PermissionStatus.granted ||
+        microphonePermission != PermissionStatus.granted) {
+      throw "카메라 또는 마이크 권한이 없습니다.";
+    }
+
+    if (engine == null) {
+      engine = createAgoraRtcEngine();
+
+      await engine!.initialize(RtcEngineContext(appId: appId));
+
+      await engine!.enableVideo();
+      await engine!.startPreview();
+
+      ChannelMediaOptions options = ChannelMediaOptions();
+
+      await engine!.joinChannel(
+          token: token, channelId: channelName, uid: uid, options: options);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("LIVE"),
       ),
-      body: Stack(
-        children: [
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            width: 120,
-            height: 160,
-            color: Colors.blue,
-          ),
-          Positioned(
-            bottom: 16,
-            left: 16,
-            right: 16,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text("나가기"),
-            ),
-          ),
-        ],
-      ),
+      body: FutureBuilder<void>(
+          future: init(),
+          builder: (context, snapshot) {
+            // FutureBuilder에 삽입된 함수에서 에러가 발생했을때
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+
+            // FutureBuilder의 함수의 처리가 아직 완료되지 않았을때
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return Stack(
+              children: [
+                Container(
+                  color: Colors.red,
+                ),
+                Container(
+                  width: 120,
+                  height: 160,
+                  child: AgoraVideoView(
+                    controller: VideoViewController(
+                      rtcEngine: engine!,
+                      canvas: VideoCanvas(
+                        uid: uid,
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 16,
+                  left: 16,
+                  right: 16,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text("나가기"),
+                  ),
+                ),
+              ],
+            );
+          }),
     );
   }
 }
