9f3063a3eebd6e7d2251086dda578925e2dc19bb
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-09 11:41:06 +0900

    Refactored calendar component.

--- calendar_schedular/lib/main.dart
+++ calendar_schedular/lib/main.dart
@@ -3,12 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
-  // runApp하기전에 initial을 먼저 하고 다른작업을 하겠다.
+  // 기존에는 ensureInitialized가 runApp에서 실행되지만
+  // runApp하기 전 다른 함수를 사용하고 싶다면 먼저 initial을 먼저 진행 후 다른 함수를 실행해야함.
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting();
 
-  runApp(const MaterialApp(
-    home: HomeScreen(),
-  ));
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
+      home: const HomeScreen(),
+    ),
+  );
 }
--- calendar_schedular/lib/screen/home_screen.dart
+++ calendar_schedular/lib/screen/home_screen.dart
@@ -1,6 +1,5 @@
-import 'package:calendar_schedular/const/color.dart';
+import 'package:calendar_schedular/component/calendar.dart';
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,69 +13,30 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final defaultBoxDecoration = BoxDecoration(
-      borderRadius: BorderRadius.circular(6),
-      border: Border.all(
-        color: Colors.grey[200]!,
-        width: 1,
-      ),
-    );
-    final defaultTextStyle = TextStyle(
-      color: Colors.grey[600],
-      fontWeight: FontWeight.w700,
-    );
-
     return Scaffold(
       body: SafeArea(
-        child: TableCalendar(
-          locale: 'ko_KR',
-          focusedDay: DateTime.now(),
-          firstDay: DateTime(1800),
-          lastDay: DateTime(3000),
-          onDaySelected: (selectedDay, focusedDay) {
-            setState(() {
-              this.selectedDay = selectedDay;
-            });
-          },
-          selectedDayPredicate: (day) {
-            if (selectedDay == null) return false;
-
-            return day.isAtSameMomentAs(selectedDay!);
-          },
-          headerStyle: const HeaderStyle(
-            formatButtonVisible: false,
-            titleCentered: true,
-            titleTextStyle: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w700,
+        child: Column(
+          children: [
+            Calendar(
+              focusedDay: DateTime.now(),
+              onDaySelected: onDaySelected,
+              selectedDayPredicate: selectedDayPredicate,
             ),
-          ),
-          calendarStyle: CalendarStyle(
-            isTodayHighlighted: true,
-            defaultDecoration: defaultBoxDecoration,
-            weekendDecoration: defaultBoxDecoration,
-            selectedDecoration: defaultBoxDecoration.copyWith(
-              border: Border.all(
-                color: primaryColor,
-                width: 1,
-              ),
-            ),
-            defaultTextStyle: defaultTextStyle,
-            weekNumberTextStyle: defaultTextStyle,
-            selectedTextStyle: defaultTextStyle.copyWith(
-              color: primaryColor,
-            ),
-            todayDecoration: defaultBoxDecoration.copyWith(
-              color: primaryColor,
-            ),
-            outsideDecoration: defaultBoxDecoration.copyWith(
-              border: Border.all(
-                color: Colors.transparent,
-              ),
-            ),
-          ),
+          ],
         ),
       ),
     );
+  }
+
+  void onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      this.selectedDay = selectedDay;
+    });
+  }
+
+  bool selectedDayPredicate(DateTime day) {
+    if (selectedDay == null) return false;
+
+    return day.isAtSameMomentAs(selectedDay!);
   }
 }
