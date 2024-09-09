0ca1e4a2a1d73178b1cf186cd482d44c2ed6d22d
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-09 10:17:59 +0900

    initial project

---
+++ calendar_schedular/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:calendar_schedular/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomeScreen(),
+  ));
+}
