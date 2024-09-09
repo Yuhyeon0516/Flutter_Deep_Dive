694aa5ed5183620e9bf39b93da7c0bbb77577184
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-09 11:15:21 +0900

    added calendar on home screen.

--- calendar_schedular/lib/screen/home_screen.dart
+++ calendar_schedular/lib/screen/home_screen.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime? selectedDay;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: TableCalendar(
+          focusedDay: DateTime.now(),
+          firstDay: DateTime(1800),
+          lastDay: DateTime(3000),
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              this.selectedDay = selectedDay;
+            });
+          },
+          selectedDayPredicate: (day) {
+            if (selectedDay == null) return false;
+
+            return day.isAtSameMomentAs(selectedDay!);
+          },
+        ),
+      ),
     );
   }
 }
