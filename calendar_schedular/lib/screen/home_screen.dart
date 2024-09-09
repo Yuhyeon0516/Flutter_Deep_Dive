cd63eced9bf369f4ad5cefd2bc5bc86e83f5ea5b
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-09 11:29:00 +0900

    styled calendar.

--- calendar_schedular/lib/screen/home_screen.dart
+++ calendar_schedular/lib/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_schedular/const/color.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -13,6 +14,18 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final defaultBoxDecoration = BoxDecoration(
+      borderRadius: BorderRadius.circular(6),
+      border: Border.all(
+        color: Colors.grey[200]!,
+        width: 1,
+      ),
+    );
+    final defaultTextStyle = TextStyle(
+      color: Colors.grey[600],
+      fontWeight: FontWeight.w700,
+    );
+
     return Scaffold(
       body: SafeArea(
         child: TableCalendar(
@@ -29,6 +42,38 @@ class _HomeScreenState extends State<HomeScreen> {
 
             return day.isAtSameMomentAs(selectedDay!);
           },
+          headerStyle: const HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+            titleTextStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          calendarStyle: CalendarStyle(
+            isTodayHighlighted: true,
+            defaultDecoration: defaultBoxDecoration,
+            weekendDecoration: defaultBoxDecoration,
+            selectedDecoration: defaultBoxDecoration.copyWith(
+              border: Border.all(
+                color: primaryColor,
+                width: 1,
+              ),
+            ),
+            defaultTextStyle: defaultTextStyle,
+            weekNumberTextStyle: defaultTextStyle,
+            selectedTextStyle: defaultTextStyle.copyWith(
+              color: primaryColor,
+            ),
+            todayDecoration: defaultBoxDecoration.copyWith(
+              color: primaryColor,
+            ),
+            outsideDecoration: defaultBoxDecoration.copyWith(
+              border: Border.all(
+                color: Colors.transparent,
+              ),
+            ),
+          ),
         ),
       ),
     );
