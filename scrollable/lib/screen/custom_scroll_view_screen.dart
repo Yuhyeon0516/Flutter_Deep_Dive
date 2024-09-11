56f7c30260c0590df29454ab45f3a825eadee857
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-11 16:34:12 +0900

    Studied CustomScrollView List.

---
+++ scrollable/lib/screen/custom_scroll_view_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:scrollable/const/colors.dart';
+
+class CustomScrollViewScreen extends StatelessWidget {
+  final List<int> numbers = List.generate(100, (index) => index);
+  CustomScrollViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBar(
+            title: Text("CustomScrollViewScreen"),
+          ),
+          renderSliverBuilderList(),
+        ],
+      ),
+    );
+  }
+
+  SliverList renderSliverBuilderList() {
+    return SliverList(
+      // ListView.builder와 동일하게 동작함
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return renderContainer(
+            color: rainbowColors[index % rainbowColors.length],
+            index: index,
+          );
+        },
+        childCount: numbers.length,
+      ),
+    );
+  }
+
+  SliverList renderSliverChildList() {
+    return SliverList(
+      // 일반적인 ListView의 children에 넣어 그려지듯
+      delegate: SliverChildListDelegate(
+        numbers
+            .map(
+              (e) => renderContainer(
+                  color: rainbowColors[e % rainbowColors.length], index: e),
+            )
+            .toList(),
+      ),
+    );
+  }
+
+  Widget renderContainer({
+    required Color color,
+    required int index,
+    double? height,
+  }) {
+    print(index);
+
+    return Container(
+      key: Key(index.toString()),
+      height: height ?? 300,
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+            fontSize: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- scrollable/lib/screen/home_screen.dart
+++ scrollable/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:scrollable/layout/main_layout.dart';
+import 'package:scrollable/screen/custom_scroll_view_screen.dart';
 import 'package:scrollable/screen/grid_view_screen.dart';
 import 'package:scrollable/screen/list_view_screen.dart';
 import 'package:scrollable/screen/reorderable_list_view_screen.dart';
@@ -32,6 +33,10 @@ class HomeScreen extends StatelessWidget {
     ScreenModel(
       builder: (_) => const ReorderableListViewScreen(),
       name: "ReorderableListViewScreen",
+    ),
+    ScreenModel(
+      builder: (_) => CustomScrollViewScreen(),
+      name: "CustomScrollViewScreen",
     ),
   ];
 
