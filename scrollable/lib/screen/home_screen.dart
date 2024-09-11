4fcd76bcda2c3037d4f586c98b0f87f002140123
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-11 16:18:36 +0900

    Studied ReorderableListView.

--- scrollable/lib/screen/home_screen.dart
+++ scrollable/lib/screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:scrollable/layout/main_layout.dart';
 import 'package:scrollable/screen/grid_view_screen.dart';
 import 'package:scrollable/screen/list_view_screen.dart';
+import 'package:scrollable/screen/reorderable_list_view_screen.dart';
 import 'package:scrollable/screen/single_child_scroll_view_screen.dart';
 
 class ScreenModel {
@@ -28,6 +29,10 @@ class HomeScreen extends StatelessWidget {
       builder: (_) => GridViewScreen(),
       name: "GridViewScreen",
     ),
+    ScreenModel(
+      builder: (_) => const ReorderableListViewScreen(),
+      name: "ReorderableListViewScreen",
+    ),
   ];
 
   HomeScreen({super.key});
@@ -38,20 +43,22 @@ class HomeScreen extends StatelessWidget {
       title: "Home",
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: screens
-              .map(
-                (e) => ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: e.builder));
-                  },
-                  child: Text(e.name),
-                ),
-              )
-              .toList(),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: screens
+                .map(
+                  (e) => ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: e.builder));
+                    },
+                    child: Text(e.name),
+                  ),
+                )
+                .toList(),
+          ),
         ),
       ),
     );
