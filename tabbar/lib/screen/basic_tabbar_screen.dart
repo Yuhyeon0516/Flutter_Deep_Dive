f2bc86022ea3d54089cef37ad96d2d2bfefea84d
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-12 19:26:44 +0900

    Studied tabbar style.

--- tabbar/lib/screen/basic_tabbar_screen.dart
+++ tabbar/lib/screen/basic_tabbar_screen.dart
@@ -11,17 +11,54 @@ class BasicTabbarScreen extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: const Text("Basic Tabbar Screen"),
-          // appbar의 하단에 생성됨
-          bottom: TabBar(
-            tabs: Tabs.map(
-              (e) => Tab(
-                icon: Icon(e.icon),
-                child: Text(e.label),
-              ),
-            ).toList(),
+          // appbar의 하단에 생성되어 이름이 bottom
+          // PreferredSize를 이용하여 TabBar의 size도 정할수있고 custom도 가능함
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(100),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TabBar(
+                  // tab의 indicator색을 설정
+                  indicatorColor: Colors.red,
+                  // tab의 사이즈로 맞춤 기본은 label
+                  indicatorSize: TabBarIndicatorSize.tab,
+                  // indicator의 weight
+                  indicatorWeight: 4,
+                  // tab이 길어졌을때 scroll이 가능하게
+                  isScrollable: true,
+                  // icon과 child의 색이 바뀜
+                  labelColor: Colors.red,
+                  // 선택되지 않은 tab의 색을 설정
+                  unselectedLabelColor: Colors.blue,
+                  // label의 style
+                  labelStyle: const TextStyle(
+                    fontWeight: FontWeight.w700,
+                  ),
+                  // 선택되지 않은 tab의 label style
+                  unselectedLabelStyle: const TextStyle(
+                    fontWeight: FontWeight.w100,
+                  ),
+                  tabs: Tabs.map(
+                    (e) => Tab(
+                      icon: Icon(e.icon),
+                      child: Text(e.label),
+                    ),
+                  ).toList(),
+                ),
+              ],
+            ),
           ),
         ),
-        body: Container(),
+        // TabBarView를 이용하여 index별 화면 정의 가능
+        body: TabBarView(
+          physics: const NeverScrollableScrollPhysics(),
+          children: Tabs.map(
+            (e) => Center(
+              child: Icon(e.icon),
+            ),
+          ).toList(),
+        ),
       ),
     );
   }
