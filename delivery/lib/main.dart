c1e27c4170f2761370a24ba34b301104e768adc4
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-12 21:27:01 +0900

    Applied font family to material app.

--- delivery/lib/main.dart
+++ delivery/lib/main.dart
@@ -12,6 +12,9 @@ class _App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Column(
