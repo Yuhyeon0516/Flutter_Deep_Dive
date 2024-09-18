77148f0f28b2a7e06cfc572a7a12b00a125b42e5
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-18 15:11:47 +0900

    Applied custom scroll view to restaurant detail screen.

--- delivery/lib/restaurant/view/restaurant_detail_screen.dart
+++ delivery/lib/restaurant/view/restaurant_detail_screen.dart
@@ -9,25 +9,81 @@ class RestaurantDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      title: '불타는 떡볶이',
-      child: Column(
-        children: [
-          RestaurantCard(
-            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-            name: '불타는 떡볶이',
-            tags: const ['떡볶이', '맛있음', '치즈'],
-            ratingsCount: 100,
-            deliveryTime: 40,
-            deliveryFee: 2000,
-            ratings: 4.52,
-            isDetail: true,
-            detail: '정말 맛있어요.',
+        title: '불타는 떡볶이',
+        child: CustomScrollView(
+          slivers: [
+            renderTop(),
+            renderLabel(),
+            renderProducts(),
+          ],
+        )
+
+        // Column(
+        //   children: [
+        //     RestaurantCard(
+        //       image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        //       name: '불타는 떡볶이',
+        //       tags: const ['떡볶이', '맛있음', '치즈'],
+        //       ratingsCount: 100,
+        //       deliveryTime: 40,
+        //       deliveryFee: 2000,
+        //       ratings: 4.52,
+        //       isDetail: true,
+        //       detail: '정말 맛있어요.',
+        //     ),
+        //     const Padding(
+        //       padding: EdgeInsets.symmetric(horizontal: 16),
+        //       child: ProductCard(),
+        //     ),
+        //   ],
+        // ),
+        );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: '불타는 떡볶이',
+        tags: const ['떡볶이', '맛있음', '치즈'],
+        ratingsCount: 100,
+        deliveryTime: 40,
+        deliveryFee: 2000,
+        ratings: 4.52,
+        isDetail: true,
+        detail: '정말 맛있어요.',
+      ),
+    );
+  }
+
+  SliverPadding renderProducts() {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return const Padding(
+              padding: EdgeInsets.only(top: 16),
+              child: ProductCard(),
+            );
+          },
+          childCount: 10,
+        ),
+      ),
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return const SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            child: ProductCard(),
-          ),
-        ],
+        ),
       ),
     );
   }
