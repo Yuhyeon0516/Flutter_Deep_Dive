abca948449bf29f7b9240350aac981c08c403848
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-20 20:46:10 +0900

    Applied riverpod provider to restaurant screen.

--- delivery/lib/restaurant/view/restaurant_screen.dart
+++ delivery/lib/restaurant/view/restaurant_screen.dart
@@ -1,25 +1,73 @@
+import 'package:delivery/common/model/cursor_pagination_model.dart';
 import 'package:delivery/restaurant/component/restaurant_card.dart';
 import 'package:delivery/restaurant/provider/restaurant_provider.dart';
 import 'package:delivery/restaurant/view/restaurant_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    // 현재 위치가 최대 길이보다 조금 덜 되는 위치까지 도달하였다면
+    // 새로운 데이터를 요청
+    if (controller.offset >
+        controller.position.maxScrollExtent -
+            MediaQuery.of(context).size.height / 2) {
+      ref.read(restaurantProvider.notifier).paginate(
+            fetchMore: true,
+          );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
-    if (data.isEmpty) {
+    if (data is CursorPaginationLoading) {
       return const Center(
         child: CircularProgressIndicator(),
       );
     }
 
+    if (data is CursorPaginationError) {
+      return Center(
+        child: Text(data.message),
+      );
+    }
+
+    final cp = data as CursorPaginationModel;
+
     return ListView.separated(
+      controller: controller,
+      itemCount: cp.data.length + 1,
       itemBuilder: (_, index) {
-        final parseItem = data[index];
+        if (index == cp.data.length) {
+          return Padding(
+            padding: const EdgeInsets.all(16),
+            child: Center(
+              child: data is CursorPaginationFetchingMore
+                  ? const CircularProgressIndicator()
+                  : const Text('마지막 데이터입니다.'),
+            ),
+          );
+        }
+
+        final parseItem = cp.data[index];
 
         return GestureDetector(
           onTap: () {
@@ -42,7 +90,6 @@ class RestaurantScreen extends ConsumerWidget {
       separatorBuilder: (_, index) {
         return const SizedBox(height: 16);
       },
-      itemCount: data.length,
     );
   }
 }
