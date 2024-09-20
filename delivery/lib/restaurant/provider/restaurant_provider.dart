c297175c77b32580327c5289376b77ce40de6588
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-20 21:33:55 +0900

    Created restaurant detail provider.

--- delivery/lib/restaurant/provider/restaurant_provider.dart
+++ delivery/lib/restaurant/provider/restaurant_provider.dart
@@ -1,7 +1,19 @@
 import 'package:delivery/common/model/cursor_pagination_model.dart';
 import 'package:delivery/common/model/pagination_params.dart';
+import 'package:delivery/restaurant/model/restaurant_model.dart';
 import 'package:delivery/restaurant/repository/restaurant_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final restaurantDetailProvider =
+    Provider.family<RestaurantModel?, String>((ref, id) {
+  final state = ref.watch(restaurantProvider);
+
+  if (state is! CursorPaginationModel) {
+    return null;
+  }
+
+  return state.data.firstWhere((e) => e.id == id);
+});
 
 final restaurantProvider =
     StateNotifierProvider<RestaurantStateNotifier, CursorPaginationBase>(
