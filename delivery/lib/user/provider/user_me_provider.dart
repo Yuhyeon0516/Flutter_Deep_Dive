3cc611cc9e5538aa15abfea6a8a930e8b26f0781
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-24 13:12:14 +0900

    Created user me provider and created function that get my information.

---
+++ delivery/lib/user/provider/user_me_provider.dart
@@ -0,0 +1,30 @@
+import 'package:delivery/common/const/data.dart';
+import 'package:delivery/user/model/user_model.dart';
+import 'package:delivery/user/repository/user_me_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
+  final UserMeRepository repository;
+  final FlutterSecureStorage storage;
+
+  UserMeStateNotifier({
+    required this.repository,
+    required this.storage,
+  }) : super(UserModelLoading()) {
+    getMe();
+  }
+
+  Future<void> getMe() async {
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    if (refreshToken == null || accessToken == null) {
+      return;
+    }
+
+    final res = await repository.getMe();
+
+    state = res;
+  }
+}
