de7e3434933239c70dde116287ab568329892ed4
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-24 13:30:01 +0900

    Created function that user login & logout.

--- delivery/lib/user/provider/user_me_provider.dart
+++ delivery/lib/user/provider/user_me_provider.dart
@@ -1,5 +1,6 @@
 import 'package:delivery/common/const/data.dart';
 import 'package:delivery/user/model/user_model.dart';
+import 'package:delivery/user/repository/auth_repository.dart';
 import 'package:delivery/user/repository/user_me_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -7,10 +8,12 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
   final UserMeRepository repository;
   final FlutterSecureStorage storage;
+  final AuthRepository authRepository;
 
   UserMeStateNotifier({
     required this.repository,
     required this.storage,
+    required this.authRepository,
   }) : super(UserModelLoading()) {
     getMe();
   }
@@ -28,5 +31,39 @@ class UserMeStateNotifier extends StateNotifier<UserModelBase?> {
     final res = await repository.getMe();
 
     state = res;
+  }
+
+  Future<UserModelBase> login({
+    required String username,
+    required String password,
+  }) async {
+    try {
+      state = UserModelLoading();
+
+      final res =
+          await authRepository.login(username: username, password: password);
+
+      await storage.write(key: REFRESH_TOKEN_KEY, value: res.refreshToken);
+      await storage.write(key: ACCESS_TOKEN_KEY, value: res.accessToken);
+
+      final userRes = await repository.getMe();
+
+      state = userRes;
+
+      return userRes;
+    } catch (e) {
+      state = UserModelError(message: '로그인 중 에러가 발생하였습니다.');
+
+      return Future.value(state);
+    }
+  }
+
+  Future<void> logout() async {
+    state = null;
+
+    await Future.wait([
+      storage.delete(key: REFRESH_TOKEN_KEY),
+      storage.delete(key: ACCESS_TOKEN_KEY)
+    ]);
   }
 }
