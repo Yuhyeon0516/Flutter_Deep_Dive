f9b75376588d89ba4c0f7ff5324b6f6b2a80e44d
Yuhyeon0516 <rladbgus0516@naver.com>
2024-09-25 16:28:22 +0900

    Applied bottom navigation cubit to bottom navigation bar on main screen.

--- e_commerce/lib/presentation/main/main_screen.dart
+++ e_commerce/lib/presentation/main/main_screen.dart
@@ -1,11 +1,29 @@
 import 'package:e_commerce/core/theme/constant/app_icons.dart';
+import 'package:e_commerce/presentation/main/cubit/bottom_navigation_cubit.dart';
+import 'package:e_commerce/presentation/pages/category/category_page.dart';
+import 'package:e_commerce/presentation/pages/home/home_page.dart';
+import 'package:e_commerce/presentation/pages/search/search_page.dart';
+import 'package:e_commerce/presentation/pages/user/user_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 
 class MainScreen extends StatelessWidget {
   static String get routeName => 'main';
 
   const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => BottomNavigationCubit(),
+      child: const MainScreenView(),
+    );
+  }
+}
+
+class MainScreenView extends StatelessWidget {
+  const MainScreenView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -56,35 +74,40 @@ class MainScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: const Center(
-        child: Text('Main Screen'),
+      body: BlocBuilder<BottomNavigationCubit, BottomNavigation>(
+        builder: (_, state) {
+          switch (state) {
+            case BottomNavigation.home:
+              return const HomePage();
+            case BottomNavigation.category:
+              return const CategoryPage();
+            case BottomNavigation.search:
+              return const SearchPage();
+            case BottomNavigation.user:
+              return const UserPage();
+          }
+        },
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(AppIcons.navHome),
-            activeIcon: SvgPicture.asset(AppIcons.navHomeOn),
-            label: 'home',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(AppIcons.navCategory),
-            activeIcon: SvgPicture.asset(AppIcons.navCategoryOn),
-            label: 'category',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(AppIcons.navSearch),
-            activeIcon: SvgPicture.asset(AppIcons.navSearchOn),
-            label: 'search',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(AppIcons.navUser),
-            activeIcon: SvgPicture.asset(AppIcons.navUserOn),
-            label: 'user',
-          ),
-        ],
+      bottomNavigationBar: BlocBuilder<BottomNavigationCubit, BottomNavigation>(
+        builder: (context, state) {
+          return BottomNavigationBar(
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            type: BottomNavigationBarType.fixed,
+            currentIndex: state.index,
+            onTap: (value) =>
+                context.read<BottomNavigationCubit>().changeIndex(value),
+            items: List.generate(
+              BottomNavigation.values.length,
+              (index) => BottomNavigationBarItem(
+                icon: SvgPicture.asset(BottomNavigation.values[index].icon),
+                activeIcon:
+                    SvgPicture.asset(BottomNavigation.values[index].activeIcon),
+                label: BottomNavigation.values[index].label,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
