579d8778569ed9f62d8e09b2ccd28c3d71364718
Yuhyeon0516 <rladbgus0516@naver.com>
2024-10-02 14:20:45 +0900

    Applied theme.

--- spotify/lib/core/configs/theme/app_theme.dart
+++ spotify/lib/core/configs/theme/app_theme.dart
@@ -4,6 +4,7 @@ import 'package:spotify/core/configs/theme/app_colors.dart';
 class AppTheme {
   static final lightTheme = ThemeData(
     brightness: Brightness.light,
+    fontFamily: 'Satoshi',
     primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.lightBackground,
     elevatedButtonTheme: ElevatedButtonThemeData(
@@ -22,6 +23,7 @@ class AppTheme {
 
   static final darkTheme = ThemeData(
     brightness: Brightness.dark,
+    fontFamily: 'Satoshi',
     primaryColor: AppColors.primary,
     scaffoldBackgroundColor: AppColors.darkBackground,
     elevatedButtonTheme: ElevatedButtonThemeData(
