4f0195b640bbf3fc388421cf6cff5718486c375a
Yuhyeon0516 <rladbgus0516@naver.com>
2024-10-09 08:43:00 +0900

    Created blog viewer page.

--- full_course/lib/features/blog/presentation/widgets/blog_card.dart
+++ full_course/lib/features/blog/presentation/widgets/blog_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:full_course/core/utils/calculate_reading_time.dart';
 import 'package:full_course/features/blog/domain/entities/blog.dart';
+import 'package:full_course/features/blog/presentation/pages/blog_viewer_page.dart';
 
 class BlogCard extends StatelessWidget {
   final BlogEntity blog;
@@ -14,43 +15,48 @@ class BlogCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      margin: const EdgeInsets.all(16),
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: blog.topics
-                  .map(
-                    (e) => Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Chip(
-                        label: Text(e),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, BlogViewerPage.route(blog));
+      },
+      child: Container(
+        height: 200,
+        margin: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: blog.topics
+                    .map(
+                      (e) => Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Chip(
+                          label: Text(e),
+                        ),
                       ),
-                    ),
-                  )
-                  .toList(),
-            ),
-          ),
-          Expanded(
-            child: Text(
-              blog.title,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 22,
+                    )
+                    .toList(),
               ),
             ),
-          ),
-          Text('${calculateReadingTime(blog.content)} min'),
-        ],
+            Expanded(
+              child: Text(
+                blog.title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            Text('${calculateReadingTime(blog.content)} min'),
+          ],
+        ),
       ),
     );
   }
